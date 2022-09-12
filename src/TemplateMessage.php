ea31d0607be11c16e523e5d1905c9e433e5cd69d
Thelt <ghiot.pierre@gmail.com>
2022-09-12 14:21:57 +0200

    Use SendInBlue message versions by default

--- src/TemplateMessage.php
+++ src/TemplateMessage.php
@@ -1,6 +1,7 @@
 <?php
 namespace Mograine\Sendinblue;
 
+use Exception;
 use SendinBlue\Client\Model\SendSmtpEmail;
 use SendinBlue\Client\Model\SendSmtpEmailReplyTo;
 use SendinBlue\Client\Model\SendSmtpEmailSender;
@@ -16,9 +17,18 @@ use SendinBlue\Client\Model\SendSmtpEmailTo;
 class TemplateMessage
 {
     private SendSmtpEmail $sendSmtpEmail;
+    private array $tags = [];
     private array $attributes = [];
+    private array $messageVersions = [];
 
-    public function __construct(int $templateId)
+    /**
+     * @param int $templateId
+     * @param bool $useMessageVersions true
+     */
+    public function __construct(
+        int $templateId,
+        private readonly bool $useMessageVersions = true
+    )
     {
         $this->sendSmtpEmail = new SendSmtpEmail();
         $this->sendSmtpEmail["templateId"] = $templateId;
@@ -26,22 +36,70 @@ class TemplateMessage
         $this->sendSmtpEmail["replyTo"] = new SendSmtpEmailReplyTo(["email" => config('sendinblue.email')]);
     }
 
+    /**
+     * @param string $email
+     * @return $this
+     * @throws Exception
+     */
     public function to(string $email): self
     {
-        $this->sendSmtpEmail->setTo([new SendSmtpEmailTo(['email' => $email])]);
+        if (!$this->useMessageVersions) {
+            $this->sendSmtpEmail->setTo([new SendSmtpEmailTo(['email' => $email])]);
+        } else {
+            $messageVersion = [];
+            $messageVersion['to'] = [
+                [
+                    'email' => $email
+                ]
+            ];
+            $this->messageVersions[] = $messageVersion;
+
+            if (count($this->messageVersions) > 2000) {
+                throw new Exception("messageVersions limit reached");
+            }
+        }
+
         return $this;
     }
 
+    /**
+     * @param string $key
+     * @param string $value
+     * @return $this
+     */
     public function attribute(string $key, string $value): self
     {
         $this->attributes[$key] = $value;
         return $this;
     }
 
+    /**
+     * @param string $tag
+     * @return $this
+     */
+    public function tag(string $tag): self
+    {
+        $this->tags[] = $tag;
+        return $this;
+    }
+
+    /**
+     * Return the SendInBlue SendSmtpEmail instance used in api->sendTransacEmail
+     *
+     * @return SendSmtpEmail
+     */
     public function getSmtpEmail(): SendSmtpEmail
     {
         if (!empty($this->attributes)) {
             $this->sendSmtpEmail['params'] = (object)$this->attributes;
+        }
+
+        if (!empty($this->tags)) {
+            $this->sendSmtpEmail['tags'] = $this->tags;
+        }
+
+        if ($this->useMessageVersions) {
+            $this->sendSmtpEmail["messageVersions"] = $this->messageVersions;
         }
 
         return $this->sendSmtpEmail;
